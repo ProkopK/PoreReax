9cd33e5cb3c34cf94b7943d42397b511d74d4637
Karl Prokop <karl.prokop@gmx.de>
2026-04-25 04:28:49 +0000

    Updated function arguments to new naming and added new samplers

--- porereax/templates/ana.py
+++ porereax/templates/ana.py
@@ -1,5 +1,4 @@
 import porereax as prx
-import matplotlib.pyplot as plt
 
 import warnings
 warnings.filterwarnings('ignore', message='.*OVITO.*PyPI')
@@ -9,9 +8,9 @@ warnings.filterwarnings('ignore', message='.*OVITO.*PyPI')
 
 num_sims = {{ NUMSIMS }}
 
-for sim in range(1, num_sims + 1):
-    bond_file = f"reax_run_{sim}"
-    traj_file = f"reax_run_{sim}"
+for sim in range(num_sims):
+    bond_file = f"reax_run_{sim}.bonds"
+    traj_file = f"reax_run_{sim}.lammpstrj"
 
     atom_lib = {{ NAME_TO_TYPE }}
     masses = {{ ATOM_MASSES }}
@@ -22,24 +21,31 @@ for sim in range(1, num_sims + 1):
         # {"atom": "b"},
         # {"atom": "c"},
         # {"atom": "a", "bonds": ["b", "b", "c"]},
-        ]
+    ]
     bonds = [
         # {"bond": "a-b", "bonds_A": ["b", "b", "b"], "bonds_B": ["c"]},
-        ]
-    sampler.add_bond_structure_sampling(f"run_{sim}_bond_structure", "BondStructure")
+    ]
+    pairs = [
+        # ({"atom": "a", "bonds": ["b"]}, {"atom": "c", "bonds": ["d"]}),
+    ]
 
-    sampler.add_charge_sampling(f"run_{sim}_charge_hist", "Histogram", atoms=atoms)
+    sampler.add_molecule_structure_sampling(f"run_{sim}_molecule_structure")
 
-    sampler.add_bond_length_sampling(f"run_{sim}_bond_length", "Histogram", bonds=bonds, num_bins=200)
+    sampler.add_charge_sampling(f"run_{sim}_charge_hist", atoms=atoms)
 
-    sampler.add_angle_sampling(f"run_{sim}_angle_all", "Histogram", atoms=atoms, num_bins=180, angle="all")
-    # sampler.add_angle_sampling(f"run_{sim}_angle_b-a-c", "Histogram", atoms=atoms, num_bins=90, angle="b-a-c")
+    sampler.add_bond_length_sampling(f"run_{sim}_bond_length", bonds=bonds, dimension="Bond Length", num_bins=200)
 
-    sampler.add_density_sampling(f"run_{sim}_density_cart_1d_z", "Cartesian1D", atoms=atoms, num_bins=800)
-    sampler.add_density_sampling(f"run_{sim}_density_cart_2d_xy", "Cartesian2D", atoms=atoms, num_bins=200, direction="xy")
-    sampler.add_density_sampling(f"run_{sim}_density_cart_1d_z_cond_ch", "Cartesian1D", atoms=atoms, num_bins=200, direction="z", conditions={"Charge": (-0.7, -0.3)})
-    sampler.add_density_sampling(f"run_{sim}_density_cart_2d_xy_cond_ang", "Cartesian2D", atoms=atoms, num_bins=200, direction="xy", conditions={"Angle": (75, 125)})
+    sampler.add_angle_sampling(f"run_{sim}_angle_all", atoms=atoms, num_bins=180, angle="all")
+    # sampler.add_angle_sampling(f"run_{sim}_angle_b-a-c", atoms=atoms, num_bins=90, angle="b-a-c")
 
-    sampler.add_density_sampling(f"run_{sim}_density_time", "Time", atoms=atoms)
+    sampler.add_density_sampling(f"run_{sim}_density_time", atoms=atoms, dimension="Time")
+    sampler.add_density_sampling(f"run_{sim}_density_cart_1d_z", atoms=atoms, dimension="Cartesian1D", num_bins=800)
+    sampler.add_density_sampling(f"run_{sim}_density_cart_2d_xy", atoms=atoms, dimension="Cartesian2D", num_bins=200, direction="xy")
+    sampler.add_density_sampling(f"run_{sim}_density_cart_1d_z_cond_ch", atoms=atoms, dimension="Cartesian1D", num_bins=200, direction="z", conditions={"Charge": (-0.7, -0.3)})
+    sampler.add_density_sampling(f"run_{sim}_density_cart_2d_xy_cond_ang", atoms=atoms, dimension="Cartesian2D", num_bins=200, direction="xy", conditions={"Angle": (75, 125)})
+
+    sampler.add_bond_density_sampling(f"run_{sim}_bond_density_cart_1d", bonds, "Cartesian1D", num_bins=800)
+
+    sampler.add_rdf_sampling(f"run_{sim}_rdf", pairs, r_max=10.0, num_bins=200
 
     sampler.sample(is_parallel=True)
