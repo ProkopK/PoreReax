1f73c86d3f37e8a9640b41954467ec63d3a2f51d
Karl Prokop <karl.prokop@gmx.de>
2026-06-26 15:41:06 +0200

    test: add simulate tests

---
+++ tests/conftest.py
@@ -0,0 +1,37 @@
+import pytest
+
+from porereax.simulate import Simulate
+from pathlib import Path
+
+TEST_DATA_DIR = Path(__file__).parent / "data"
+gro_lib = {
+    "OM": "O",
+    "SI": "Si",
+    "Si": "Si",
+    "O": "O",
+    "H": "H",
+    "OW": "O",
+    "HW": "H",
+    "MW": "",
+}
+gro_charges = {
+    "OM": -0.64,
+    "SI": 1.28,
+    "Si": 1.28,
+    "O": -0.74,
+    "H": 0.42,
+    "OW": -1.1128,
+    "HW": 0.5564,
+}
+atom_masses = {"Si": 28.086, "O": 15.9994, "H": 2.016}
+
+
+@pytest.fixture
+def simulate_pore(tmp_path, monkeypatch):
+    monkeypatch.chdir(tmp_path)
+    return Simulate(
+        gro_lib=gro_lib,
+        gro_charges=gro_charges,
+        atom_masses=atom_masses,
+        structure_file=TEST_DATA_DIR / "test_structure.gro",
+    )
