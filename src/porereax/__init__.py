ebb644047e98beedb475ad5d9521d4f47f3e2985
Karl Prokop <karl.prokop@gmx.de>
2026-07-29 16:35:26 +0200

    chore: update __init__ to export all functions without calling a module

--- src/porereax/__init__.py
+++ src/porereax/__init__.py
@@ -1,21 +1,22 @@
 """
-PoreReax is a Python package designed for analyzing and setting up reactive molecular dynamics simulations using the ReaxFF force field. It provides tools for setting up simulations, sampling various molecular properties, and visualizing the results.
+PoreReax is a Python package designed for analysing and setting up reactive molecular dynamics simulations using the ReaxFF force field. It provides tools for setting up simulations, sampling various molecular properties, and visualizing the results.
 """
 
 
-import porereax.plot as plot
-import porereax.utils as utils
-
 from porereax.sample import Sample
 from porereax.simulate import Simulate
+from porereax.plot import plot
+from porereax.utils import load_object, get_identifiers, get_data
 
 
 __version__ = "0.0.1"
 
 __all__ = [
-    "utils",
-    "plot",
     "Sample",
     "Simulate",
+    "plot",
+    "load_object",
+    "get_identifiers",
+    "get_data",
     "__version__",
 ]
